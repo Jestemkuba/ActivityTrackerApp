4add53aa42675e87819919ffcf1c7aaa91891ae6
DESKTOP-NV6T94D\Kuba <kuba.lobacz@gmail.com>
2021-05-09 21:31:09 +0200

    Create AuthService

---
+++ ActivityTrackerApp/ActivityTrackerApp/Models/LoginResult.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityTrackerApp.Models
+{
+    class LoginResult
+    {
+        public bool LoginSuccessful { get; set; }
+    }
+}
