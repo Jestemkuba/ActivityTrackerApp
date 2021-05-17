a8077ac8d5b433ccb60ebc4041bd2e996c68d6c5
DESKTOP-NV6T94D\Kuba <kuba.lobacz@gmail.com>
2021-05-18 00:28:22 +0200

    Add models

---
+++ ActivityTrackerApp/ActivityTrackerApp/Models/DTOs/RegisterRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityTrackerApp.Models.DTOs
+{
+    public class RegisterRequestDto
+    {
+        public string Email { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ ActivityTrackerApp/ActivityTrackerApp/Models/RegisterResult.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityTrackerApp.Models
+{
+    public class RegisterResult
+    {
+        public bool IsSuccesful { get; set; }
+        public string Message { get; set; }
+    }
+}
