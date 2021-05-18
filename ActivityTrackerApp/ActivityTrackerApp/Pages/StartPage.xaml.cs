520317f70a5d1768063209eeb437eeaaab2825d1
DESKTOP-NV6T94D\Kuba <kuba.lobacz@gmail.com>
2021-05-18 21:37:08 +0200

    Add StartPage and viewmodel

---
+++ ActivityTrackerApp/ActivityTrackerApp/Pages/StartPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using ActivityTrackerApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ActivityTrackerApp.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StartPage : ContentPage
+    {
+        public StartPage()
+        {
+            InitializeComponent();
+            BindingContext = App.Resolve<StartViewModel>();
+        }
+    }
+}
---
+++ ActivityTrackerApp/ActivityTrackerApp/ViewModels/StartViewModel.cs
@@ -0,0 +1,26 @@
+﻿using ActivityTrackerApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityTrackerApp.ViewModels
+{
+    class StartViewModel : BaseViewModel
+    {
+        private readonly IAuthService _authService;
+        private string _username;
+
+        public StartViewModel(IAuthService authService)
+        {
+            _authService = authService;
+            Username = _authService.User.Username;
+        }
+
+        public string Username 
+        {
+            get => _username;
+            set => SetProperty(ref _username, value);
+        }
+
+    }
+}
