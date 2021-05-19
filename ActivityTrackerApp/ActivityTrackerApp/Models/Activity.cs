e45eef86c98ca58104c1b775ad91beeaf751620e
DESKTOP-NV6T94D\Kuba <kuba.lobacz@gmail.com>
2021-05-19 02:18:14 +0200

    Add Activity model

---
+++ ActivityTrackerApp/ActivityTrackerApp/Models/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ActivityTrackerApp.Models
+{
+    public class Activity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Distance { get; set; }
+        public double MovingTime { get; set; }
+        public double ElapsedTime { get; set; }
+        public string Type { get; set; }
+        public double AverageSpeed { get; set; }
+        public double MaxSpeed { get; set; }
+    }
+}
