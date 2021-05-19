9f96fc14532b4c3e9145786015ea53820dc2fa79
DESKTOP-NV6T94D\Kuba <kuba.lobacz@gmail.com>
2021-05-19 02:59:21 +0200

    Create ActivitiesViewModel

---
+++ ActivityTrackerApp/ActivityTrackerApp/ViewModels/ActivitiesViewModel.cs
@@ -0,0 +1,33 @@
+﻿using ActivityTrackerApp.Models;
+using ActivityTrackerApp.Services;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityTrackerApp.ViewModels
+{
+    public class ActivitiesViewModel : BaseViewModel
+    {
+        private readonly IActivityService _activityService;
+        private ObservableCollection<Activity> _activities;
+
+        public ActivitiesViewModel(IActivityService activityService)
+        {
+            _activityService = activityService;
+        }
+
+        public ObservableCollection<Activity> Activities 
+        {
+            get => _activities;
+            set => SetProperty(ref _activities, value);
+        }
+
+        public override Task Initialize()
+        {
+            Activities = _activityService.Activities;
+            return base.Initialize();
+        }
+    }
+}
