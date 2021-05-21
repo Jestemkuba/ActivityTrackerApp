1a938ce3f5b3dc329b291c62147bcb6f3920af95
DESKTOP-NV6T94D\Kuba <kuba.lobacz@gmail.com>
2021-05-21 23:15:17 +0200

    Add Activity property

--- ActivityTrackerApp/ActivityTrackerApp/Views/ActivityFullDetailsView.xaml.cs
+++ ActivityTrackerApp/ActivityTrackerApp/Views/ActivityFullDetailsView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ActivityTrackerApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,67 +18,13 @@ namespace ActivityTrackerApp.Views
             InitializeComponent();
         }
 
-        public string Name
+        public Activity Activity 
         {
-            get { return (string)GetValue(NameProperty); }
-            set { SetValue(NameProperty, value); }
+            get => (Activity)GetValue(ActivityProperty);
+            set => SetValue(ActivityProperty, value);
         }
 
-        public double Distance
-        {
-            get { return (double)GetValue(DistanceProperty); }
-            set { SetValue(DistanceProperty, value); }
-        }
-
-        public double MovingTime
-        {
-            get { return (double)GetValue(MovingTimeProperty); }
-            set { SetValue(MovingTimeProperty, value); }
-        }
-
-        public double ElapsedTime
-        {
-            get { return (double)GetValue(ElapsedTimeProperty); }
-            set { SetValue(ElapsedTimeProperty, value); }
-        }
-
-        public string Type
-        {
-            get { return (string)GetValue(TypeProperty); }
-            set { SetValue(TypeProperty, value); }
-        }
-
-        public double AverageSpeed
-        {
-            get { return (double)GetValue(AverageSpeedProperty); }
-            set { SetValue(AverageSpeedProperty, value); }
-        }
-
-        public double MaxSpeed
-        {
-            get { return (double)GetValue(MaxSpeedProperty); }
-            set { SetValue(MaxSpeedProperty, value); }
-        }
-
-        public static readonly BindableProperty NameProperty =
-         BindableProperty.Create("Name", typeof(string), typeof(ActivityFullDetailsView), null);
-
-        public static readonly BindableProperty DistanceProperty =
-         BindableProperty.Create("Distance", typeof(double), typeof(ActivityFullDetailsView), default(double));
-
-        public static readonly BindableProperty MovingTimeProperty =
-         BindableProperty.Create("MovingTime", typeof(double), typeof(ActivityFullDetailsView), default(double));
-
-        public static readonly BindableProperty ElapsedTimeProperty =
-         BindableProperty.Create("ElapsedTime", typeof(double), typeof(ActivityFullDetailsView), default(double));
-
-        public static readonly BindableProperty TypeProperty =
-         BindableProperty.Create("Type", typeof(string), typeof(ActivityFullDetailsView),null);
-
-        public static readonly BindableProperty AverageSpeedProperty =
-         BindableProperty.Create("AverageSpeed", typeof(double), typeof(ActivityFullDetailsView), default(double));
-
-        public static readonly BindableProperty MaxSpeedProperty =
-         BindableProperty.Create("MaxSpeed", typeof(double), typeof(ActivityFullDetailsView), default(double));
+        public static readonly BindableProperty ActivityProperty =
+            BindableProperty.Create("Activity", typeof(Activity), typeof(ActivityFullDetailsView), null);
     }
 }
