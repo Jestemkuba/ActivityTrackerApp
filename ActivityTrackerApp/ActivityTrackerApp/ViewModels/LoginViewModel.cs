b0bcad779a71eec8a939be5b9aa4b84800e86ff5
DESKTOP-NV6T94D\Kuba <kuba.lobacz@gmail.com>
2021-05-23 12:03:10 +0200

    Add properties

--- ActivityTrackerApp/ActivityTrackerApp/ViewModels/LoginViewModel.cs
+++ ActivityTrackerApp/ActivityTrackerApp/ViewModels/LoginViewModel.cs
@@ -14,11 +14,13 @@ namespace ActivityTrackerApp.ViewModels
     public class LoginViewModel : BaseViewModel
     {
         private readonly IAuthService _authService;
-        private readonly IActivityService _activityService;
-        private ICommand _loginCommand;
-        private ICommand _goToRegisterCommand;
+        private readonly IActivityService _activityService;      
         private string _username;
         private string _password;
+        private bool _validationMessageVisible;
+        private bool _isLogging;
+        private ICommand _loginCommand;
+        private ICommand _goToRegisterCommand;
 
         public LoginViewModel(IAuthService authService, IActivityService activityService)
         {
@@ -34,14 +36,29 @@ namespace ActivityTrackerApp.ViewModels
             get => _username;
             set => SetProperty(ref _username, value);
         }
+
         public string Password
         {
             get => _password;
             set => SetProperty(ref _password, value);
         }
 
+        public bool ValidationMessageVisible 
+        {
+            get => _validationMessageVisible;
+            set => SetProperty(ref _validationMessageVisible, value);
+        }
+
+        public bool IsLogging 
+        {
+            get => _isLogging;
+            set => SetProperty(ref _isLogging, value);
+        }
+
         private async void Login()
         {
+            ValidationMessageVisible = false;
+            IsLogging = true;
             var request = new LoginRequestDto
             {
                 Username = Username,
@@ -53,7 +70,12 @@ namespace ActivityTrackerApp.ViewModels
             {
                 await Shell.Current.GoToAsync("///StartPage");
             }
-               
+            else
+            {
+                ValidationMessageVisible = true;
+            }
+
+            IsLogging = false;            
         }
 
         private async void GoToRegister()
