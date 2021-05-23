a9128926590676d5269f8944b85d220b0895555e
DESKTOP-NV6T94D\Kuba <kuba.lobacz@gmail.com>
2021-05-23 12:01:46 +0200

    Create ValueConverters

---
+++ ActivityTrackerApp/ActivityTrackerApp/ValueConverters/NegatedBoolValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace ActivityTrackerApp.ValueConverters
+{
+    class NegatedBoolValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
