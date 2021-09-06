22949c50d40a931e8b189122bce7cd3591559f4a
DESKTOP-NV6T94D\Kuba <kuba.lobacz@gmail.com>
2021-09-06 22:05:11 +0200

    Add task extension method used to await task

---
+++ ActivityTrackerApp/ActivityTrackerApp/Utilities/TaskUtilities.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivityTrackerApp.Utilities
+{
+    public static class TaskUtilities
+    {
+        public async static void Await(this Task task, Action completedCallback = null, Action<Exception> errorCallback = null)
+        {
+            try
+            {
+                await task;
+                completedCallback?.Invoke();
+            }
+            catch (Exception ex)
+            {
+                errorCallback?.Invoke(ex);
+            }
+        }
+    }
+}
