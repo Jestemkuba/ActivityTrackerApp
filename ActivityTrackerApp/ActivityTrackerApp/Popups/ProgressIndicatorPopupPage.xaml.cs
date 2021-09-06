34db9e6941216a225b225ba55880422e3328cba2
DESKTOP-NV6T94D\Kuba <kuba.lobacz@gmail.com>
2021-09-06 20:38:52 +0200

    Create ProgressIndicatorPopupPage

---
+++ ActivityTrackerApp/ActivityTrackerApp/Popups/ProgressIndicatorPopupPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Rg.Plugins.Popup.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ActivityTrackerApp.Popups
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ProgressIndicatorPopupPage : PopupPage
+    {
+        public ProgressIndicatorPopupPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
